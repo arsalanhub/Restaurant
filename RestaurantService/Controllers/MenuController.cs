4b018aeaadc576d326437f0188aa8579b163a6ca
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-07-18 11:11:58 +0530

    Menu Controller added

---
+++ RestaurantService/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantService.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
