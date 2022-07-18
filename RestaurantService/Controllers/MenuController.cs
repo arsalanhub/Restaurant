003bf53d574688bcccfccd4c28900507a5de3ca9
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-07-18 11:35:43 +0530

    Display all data

--- RestaurantService/Controllers/MenuController.cs
+++ RestaurantService/Controllers/MenuController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantService.Data;
+using RestaurantService.Models;
 
 namespace RestaurantService.Controllers
 {
     public class MenuController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        [ActivatorUtilitiesConstructor]
+        public MenuController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Menu> objCategoryList = _db.Menus;
+            return View(objCategoryList);
         }
     }
 }
--- RestaurantService/Data/ApplicationDbContext.cs
+++ RestaurantService/Data/ApplicationDbContext.cs
@@ -9,5 +9,10 @@ namespace RestaurantService.Data
         {
         }
         public DbSet<Menu> Menus { get; set; }
+
+        public static implicit operator ApplicationDbContext(ApplicationBuilder v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
